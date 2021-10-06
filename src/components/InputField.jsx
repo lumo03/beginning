a50db37c23eae61d125bd4bcda81bc577f523fd8
lumo03 <luis.moncada.mso@gmail.com>
2021-10-06 18:00:12 +0200

    v. 1.0 (read the changelog for further information)

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function InputField(props) {
+    let bgColor = props.bgColor ? props.bgColor : "bg-white";
+    let styling = "m-1 w-60 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring"+" "+bgColor;
     return (
-        <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} value={props.value} className="m-1 w-60 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-
-         rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring" />
+        <input type={props.type} placeholder={props.placeholder} onChange={props.onChange} value={props.value} className={styling} />
     )
 }
 
--- src/pages/Temperature.jsx
+++ src/pages/Temperature.jsx
@@ -13,23 +13,45 @@ function Temperature() {
     const handleChange = (rechts, wert) => {
         if (rechts !== true) {
             setCelsius(wert);
-            setFahrenheit(((wert * 9/5) + 32).toFixed(2));
+            setFahrenheit(((wert * 9 / 5) + 32).toFixed(2));
         } else {
             setFahrenheit(wert);
-            setCelsius(((wert - 32) * 5/9).toFixed(2));
+            setCelsius(((wert - 32) * 5 / 9).toFixed(2));
         }
     };
+
+    const setBgColor = () => {
+        if (celsius < 0) {
+
+        }
+        switch (true) {
+            case celsius < 0:
+                return "bg-blue-600";
+                break;
+            case celsius < 24:
+                return "bg-blue-400";
+                break;
+            case celsius > 48:
+                return "bg-red-600";
+                break;
+            case celsius > 24:
+                return "bg-red-400";
+                break;
+            default:
+                return "bg-white";
+        }
+    }
 
     return (
         <Page title="Temperature" type="temperature">
             <div className="celsius">
-                <InputField placeholder="Celsius" value={celsius} onChange={(event) => handleChange(false, event.target.value)} />
-            <span className="m-2">in ° C</span>
+                <InputField placeholder="Celsius" value={celsius} onChange={(event) => handleChange(false, event.target.value)} bgColor={setBgColor()} />
+                <span className="m-2">in ° C</span>
             </div>
-            
+
             <div className="fahrenheit">
-            <InputField placeholder="Fahrenheit" value={fahrenheit} onChange={(event) => handleChange(true, event.target.value)} />
-            <span className="m-2">in ° F</span>
+                <InputField placeholder="Fahrenheit" value={fahrenheit} onChange={(event) => handleChange(true, event.target.value)} bgColor={setBgColor()} />
+                <span className="m-2">in ° F</span>
             </div>
         </Page>
     )
